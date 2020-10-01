53919569964a0a4a18ddbffa10afb257ce05a3eb
kk741847 <72181282+kk741847@users.noreply.github.com>
2020-10-01 09:39:11 +0530

    Update sortList.py

--- sortList.py
+++ sortList.py
@@ -1,6 +1,6 @@
 a=[]
 print("Enter 0 for exit and print")
-d = int(input("Enter no. one by one : "));
+d = int(input("Enter no. one by one : "))
 a.append(d)
 while(d!=0):
     d = int(input("Enter no. one by one : "))
@@ -10,6 +10,6 @@ print("original order : ",end=" ")
 for i in range (0,len(a)):
     printf(a[i],end=" ")
 a.sort()
-print(" & in Ascending order : ",end=" ");
+print(" & in Ascending order : ",end=" ")
 for i in range (0,len(a)):
     printf(a[i],end=" ")
